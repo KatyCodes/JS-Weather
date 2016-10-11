50d277b079acfab9656fd7de6c69f79dff3a7ad8
Jena SanCartier and Katy Henning <katy@>
2016-10-11 15:40:10 -0700

    add initial files and one api link

---
+++ tmp/allConcat.js
@@ -0,0 +1,11 @@
+var apiKey = "1ea31f7b3cffdb1c84adff1cc12c4c13";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+  });
+});
